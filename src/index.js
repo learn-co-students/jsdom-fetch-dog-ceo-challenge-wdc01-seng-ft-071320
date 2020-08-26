9f00601e0de05c9f74a9263425e17ef534f0a9cd
slee713 <steven.lee1393@gmail.com>
2020-08-26 16:35:24 -0400

    combined logic for addBreed

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function addBreedsToUrl(letter = "all") {
 
     function addBreed(breeds) {
         for ( const breed in breeds.message) {
-            if (letter != "all" && breed[0] === letter) {
+            if (letter != "all" && breed[0] === letter || letter == "all") {
                 let li = document.createElement('li');
                 li.innerText = breed
                 li.setAttribute("data-breed", "general")
@@ -44,24 +44,26 @@ function addBreedsToUrl(letter = "all") {
                     })
                 }
                 
-            } else if (letter == "all") {
-                let li = document.createElement('li');
-                li.innerText = breed
-                li.setAttribute("data-breed", "general")
-                ul.append(li)
-                if (breeds.message[breed].length > 0){
-                    let innerUl = document.createElement("ul")
-                    li.append(innerUl)
+            } 
+
+            // else if (letter == "all") {
+            //     let li = document.createElement('li');
+            //     li.innerText = breed
+            //     li.setAttribute("data-breed", "general")
+            //     ul.append(li)
+            //     if (breeds.message[breed].length > 0){
+            //         let innerUl = document.createElement("ul")
+            //         li.append(innerUl)
                     
-                    breeds.message[breed].forEach( specificBreed => {
-                        let innerLi = document.createElement('li')
-                        innerLi.innerText = specificBreed
-                        innerLi.setAttribute("data-breed", "specific")
-                        innerUl.append(innerLi)
-                    })
-                }
+            //         breeds.message[breed].forEach( specificBreed => {
+            //             let innerLi = document.createElement('li')
+            //             innerLi.innerText = specificBreed
+            //             innerLi.setAttribute("data-breed", "specific")
+            //             innerUl.append(innerLi)
+            //         })
+            //     }
                 
-            }
+            // }
         }
     }
 }
